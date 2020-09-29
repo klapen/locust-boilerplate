af026bbeb7b25484739b9e16c059140e169f2d17
Orlando Saavedra <orlando@homerlearning.com>
2020-09-29 11:41:25 -0500

    Remove unused method

--- src/load_test.py
+++ src/load_test.py
@@ -4,15 +4,6 @@ from locust import HttpUser, task, between
 class BoilerplateUser(HttpUser):
     wait_time = between(1, 2)
 
-    def get_verbs_function(self, verb):
-        functions = {
-            'get'    : self.client.get,
-            'post'   : self.client.post,
-            'put'    : self.client.put,
-            'delete' : self.client.delete,
-        }
-        return functions[verb]
-
     @task
     def urls_test(self):
         with open('./urls.csv', 'r' ) as csv_file:
