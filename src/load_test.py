66a5a56590a20672f0df536df726c0dc16f52458
Orlando Saavedra <orlando@homerlearning.com>
2020-09-29 09:56:22 -0500

    Load test URLs from CSV file

--- src/load_test.py
+++ src/load_test.py
@@ -1,10 +1,30 @@
-import time
+import time, csv, json
 from locust import HttpUser, task, between
 
 class BoilerplateUser(HttpUser):
     wait_time = between(1, 2)
 
+    def get_verbs_function(self, verb):
+        functions = {
+            'get'    : self.client.get,
+            'post'   : self.client.post,
+            'put'    : self.client.put,
+            'delete' : self.client.delete,
+        }
+        return functions[verb]
+
     @task
-    def index_page(self):
-        self.client.get("/v1")
+    def urls_test(self):
+        with open('./urls.csv', 'r' ) as csv_file:
+            reader = csv.DictReader(csv_file, delimiter=',', quotechar='"')
+            for line in reader:
+                headers = line['headers']
+                payload = line['payload']
+                res = self.client.request(
+                    method  = line['method'].lower(),
+                    url     = line['url'].lower(),
+                    headers = json.loads(headers) if headers else '',
+                    data    = json.loads(payload) if payload else '',
+                )
+                print('Response status code:', res.status_code)
 
